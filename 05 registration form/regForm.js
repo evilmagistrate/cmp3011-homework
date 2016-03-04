7761be48d6b6ee75d0dde64b5ac9ff67b1c91554
Ralph Emrick <qwerty.ralph@gmail.com>
2016-03-03 19:10:18 -0500

    05_bug at password
    Validation of name and e-mail is ‘working’ but it stops behaving
    properly at password… >_<;
    Need to re-examine logic tomorrow

--- 05 registration form/regForm.js
+++ 05 registration form/regForm.js
@@ -1,82 +1,78 @@
-var fullName = document.getElementById('fullname').value;
-var email = document.getElementById('email').value;
-var password = document.getElementById('password').value;
-var retypePassword = document.getElementById('retype_password').value;
-
-var fullNameError = document.getElementById('fullname_error').textContent;
-var emailError = document.getElementById('email_error').textContent;
-var passwordError = document.getElementById('password_error').textContent;
-var retypePasswordError = document.getElementById('retype_password_error').textContent;
-
-var fullNameErrorMessage = "Must be more than 2 characters long" ;
-var emailErrorMessage = "Must be longer than 5 characters" ;
-var passwordErrorMessage = "Must be more than 10 characters long and contain a number";
-var retypePasswordErrorMessage = "Password does not match";
-var radioErrorMessage = "Password does not match";
-
 function validateForm ( ) {
 
-        if ((fullName.length == null) || (fullName.length < 2))
+    var fullName = document.getElementById('fullname').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var retypePassword = document.getElementById('retype_password').value;
 
-        {
-            console.log("return: false") ;
+    var fullNameError = document.getElementById('fullname_error').textContent;
+    var emailError = document.getElementById('email_error').textContent;
+    var passwordError = document.getElementById('password_error').textContent;
+    var retypePasswordError = document.getElementById('retype_password_error').textContent;
+
+    var fullNameErrorMessage = "Must be more than 2 characters long" ;
+    var emailErrorMessage = "Must be longer than 5 characters" ;
+    var passwordErrorMessage = "Must be more than 10 characters long and contain a number";
+    var retypePasswordErrorMessage = "Password does not match";
+
+    var alphaNumeric = /^[a-z0-9]+$/i;
+
+
+    if (fullName.length <= 2 ) {
+            console.log("fullName: false") ;
             document.getElementById('fullname_error').textContent = fullNameErrorMessage ;
             return false ;
         }
 
-        //var radioValue = defineRadio (radiofield) ;
-        //    switch(radioValue)
-        //
-        //    {
-        //        case "Blue":
-        //            document.body.style.backgroundColor = "#0bb3d0";
-        //            break;
-        //
-        //        case "Green":
-        //            document.body.style.backgroundColor = "#b4d433" ;
-        //            break;
-        //
-        //        case "Pink":
-        //            document.body.style.backgroundColor = "#ff0099" ;
-        //            break;
-        //
-        //        default:
-        //
-        //    }
-        //
+        if (email.length < 5) {
+            console.log("email: false") ;
+            document.getElementById('email_error').textContent = emailErrorMessage ;
+            return false ;
+        }
+
+        if ((password <= 10 ) && !(alphaNumeric.test(password))) {
+            console.log("password: false") ;
+            document.getElementById('password_error').textContent = passwordErrorMessage ;
+            return false ;
+        }
+
+        if (retypePassword !== password) {
+            console.log("retype password: false") ;
+            document.getElementById('retypePasswordError').textContent = retypePasswordErrorMessage ;
+            return false ;
+        }
+
         else {
             console.log("return: true") ;
-            return true;
+            //document.getElementById('result').textContent = ("<h1>Thank you for joining, <br />" + fullName + "! <h1> <p>Please remember your log-in information for future discussions. <br />" + email + "<br />" + password + "</p>" ) ;
+            return (true) ;
         }
 }
 
-
-
-//Functions to validate radio button
-
-function defineRadio (color)
-
-{
-    for (i = 0; i < color.length; ++ i)
-    {
-        if (color [i].checked) return true;
-    }
-    return false;
-}
-
-function validateRadio()
-
-{
-    if(defineRadio (document.forms["submission_form"]["color"]))
-
-    {
-        return true;
-    }
-
-    else
-
-    {
-       document.getElementById('radio_error').innerHTML='Please select a color' ;
-        return false;
-    }
-}
+//function defineRadio (color)
+//
+//{
+//    for (i = 0; i < color.length; ++ i)
+//    {
+//        if (color [i].checked) return true;
+//    }
+//    return false;
+//}
+//
+//var radioValue = defineRadio(color) ;
+//
+//switch(radioValue) {
+//    case "Blue":
+//        document.body.style.backgroundColor = "#0bb3d0";
+//        break;
+//
+//    case "Green":
+//        document.body.style.backgroundColor = "#b4d433" ;
+//        break;
+//
+//    case "Pink":
+//        document.body.style.backgroundColor = "#ff0099" ;
+//        break;
+//
+//    default:
+//}
