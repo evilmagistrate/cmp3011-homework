648b8d60e12740b56e3461a1bf47d096ad6cc62b
Ralph Emrick <qwerty.ralph@gmail.com>
2016-03-13 11:40:58 -0400

    Edits to 05 (incorporating answer key)
    Having problems with functionality, need to re-asses.

--- 05 registration form/regForm.js
+++ 05 registration form/regForm.js
@@ -1,15 +1,20 @@
+function checkLength ( fieldValue, minimumCharacterLength ) {
+
+    if ( fieldValue.length < minimumCharacterLength ) {
+        return false ;
+    }
+
+    return true ;
+
+}
+
 function validateForm ( ) {
 
     var fullName = document.getElementById('fullname').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var retypePassword = document.getElementById('retype_password').value;
-    var radioColor = document.getElementsByName('color');
-
-    var fullNameError = document.getElementById('fullname_error').textContent;
-    var emailError = document.getElementById('email_error').textContent;
-    var passwordError = document.getElementById('password_error').textContent;
-    var retypePasswordError = document.getElementById('retype_password_error').textContent;
+    var radioColor = document.getElementsByName('color').value;
 
     var fullNameErrorMessage = "Must be more than 2 characters long";
     var emailErrorMessage = "Must be longer than 5 characters";
@@ -18,20 +23,18 @@ function validateForm ( ) {
 
     var alphaNumeric = /^.*(?=.*\d)(?=.*[a-zA-Z]).*$/;
 
+    var errorStyle = "red 2px solid";
 
-    function assesRadioValue() {
-        for (var i = 0; i < radioColor.length; i++) {
-            if (radioColor[i].checked) {
-                radioInputValue = radioColor[i].value;
-            }
-        }
-    }
+    var containsError = false;
 
-    if (fullName.length <= 2) {
-        console.log("fullName: false");
+
+// username
+
+    if (!checkLength(fullName, 2)) {
+        console.log("fullName: error");
         document.getElementById('fullname_error').textContent = fullNameErrorMessage;
-        document.getElementById('fullname').style.border = "red 2px solid";
-        //return false;
+        document.getElementById('fullname').style.border = errorStyle;
+        containsError = true;
     }
 
     else {
@@ -40,11 +43,14 @@ function validateForm ( ) {
 
     }
 
-    if (email.length < 5) {
-        console.log("email: false");
+
+// email
+
+    if (!checkLength(email, 5)) {
+        console.log("email: error");
         document.getElementById('email_error').textContent = emailErrorMessage;
-        document.getElementById('email').style.border = "red 2px solid";
-        //return false;
+        document.getElementById('email').style.border = errorStyle;
+        containsError = true;
     }
 
     else {
@@ -52,99 +58,157 @@ function validateForm ( ) {
         document.getElementById('email').style.border = "";
     }
 
-    if (!alphaNumeric.test(password)) {
-        console.log("password alpha: false");
+
+// password length
+
+    var containsNumber = true;
+
+    if (!checkLength(password.value, 10)) {
+        console.log("password length: error");
         document.getElementById('password_error').textContent = passwordErrorMessage;
-        document.getElementById('password').style.border = "red 2px solid";
-        //return false;
+        document.getElementById('password').style.border = errorStyle;
+        containsError = true;
+        containsNumber = false;
     }
 
     else {
         document.getElementById('password_error').textContent = "";
         document.getElementById('password').style.border = "";
-    }
-
-    if (password.length <= 10) {
-        console.log("password length: false");
-        document.getElementById('password_error').textContent = passwordErrorMessage;
-        document.getElementById('password').style.border = "red 2px solid";
-        //return false;
-    }
-
-    else {
-        document.getElementById('password_error').textContent = "";
-        document.getElementById('password').style.border = "";
-    }
-
-    if (retypePassword !== password) {
-        console.log("retype password: false");
-        document.getElementById('retype_password_error').textContent = retypePasswordErrorMessage;
-        document.getElementById('retype_password').style.border = "red 2px solid";
-        //return false;
-    }
-
-    else {
-        document.getElementById('password_error').textContent = "";
-        document.getElementById('password').style.border = "";
-    }
-
-    var radioValue;
-
-    if ((document.getElementById('blue').checked == false) && (document.getElementById('green').checked == false) && (document.getElementById('pink').checked == false) && (document.getElementById('black').checked == false)) {
-        console.log("radio button: false");
-        document.getElementById('radio_error').textContent = "Please make a selection";
-        document.getElementById('radio_selection').style.border = "red 2px solid";
-        return false;
-    }
-
-    if (document.getElementById('blue').checked) {
-        radioValue = document.getElementById('blue').value;
-        document.body.style.backgroundColor = "#0bb3d0";
+        containsNumber = true;
 
     }
 
-    else if (document.getElementById('green').checked) {
-        radioValue = document.getElementById('green').value;
-        document.body.style.backgroundColor = "#b4d433";
 
-    }
+// password alphanumeric
 
-    else if (document.getElementById('pink').checked) {
-        radioValue = document.getElementById('pink').value;
-        document.body.style.backgroundColor = "#ff0099";
+    var passwordMatches = true;
 
-    }
+    if (containsNumber) {
 
-    else if (document.getElementById('black').checked) {
-        radioValue = document.getElementById('black').value;
-        document.body.style.backgroundColor = "#000";
+        if (!alphaNumeric.test(password)) {
+            console.log("password alpha: error");
+            document.getElementById('password_error').textContent = passwordErrorMessage;
+            document.getElementById('password').style.border = errorStyle;
+            containsError = true;
+            passwordMatches = false;
+        }
 
-    }
+        else {
+            document.getElementById('password_error').textContent = "";
+            document.getElementById('password').style.border = "";
+            passwordMatches = true;
 
-    else {
-        console.log("return: true");
-        document.getElementById('result').textContent = "<h1>Thank you for joining, <br />" + fullName + "! <h1> <p>Please remember your log-in information for future discussions. <br />" + email + "<br />" + password + "</p>";
-        return true;
-
-        switch (radioValue) {
-
-            case "Blue":
-                document.body.style.backgroundColor = "#0bb3d0";
-                break;
-
-            case "Green":
-                document.body.style.backgroundColor = "#b4d433";
-                break;
-
-            case "Pink":
-                document.body.style.backgroundColor = "#ff0099";
-                break;
-
-            case "Black":
-                document.body.style.backgroundColor = "#000";
-                break;
-
-            default:
         }
     }
+
+// retype password
+
+    if (passwordMatches) {
+        if (retypePassword !== password) {
+            console.log("retype password: error");
+            document.getElementById('retype_password_error').textContent = retypePasswordErrorMessage;
+            document.getElementById('retype_password').style.border = errorStyle;
+            containsError = true;
+        }
+
+        else {
+            document.getElementById('password_error').textContent = "";
+            document.getElementById('password').style.border = "";
+        }
+    }
+
+
+// radio button
+
+    var colorSelected = false;
+
+
+    for (var button = 0; button < radioColor.length; button++) {
+        if (radioColor[button].checked) {
+            radioInputValue = radioColor[button].value;
+            colorSelected = true;
+        }
+    }
+
+    if (! colorSelected) {
+        document.getElementById('radio_error').textContent = "Please make a selection";
+        document.getElementById('radio_selection').style.border = errorStyle;
+        containsError = true;
+    }
+
+    else {
+        document.getElementById('radio_selection').style.border = "";
+        document.getElementById('radio_error').textContent = "";
+    }
+
+    if (! containsError) {
+        console.log("submission complete");
+        document.getElementById('result').textContent = "<h1>Thank you for joining, <br />" + fullName + "! <h1> <p>Please remember your log-in information for future discussions. <br />" + email + "<br />" + password + "</p>";
+
+        document.body.style.backgroundColor = radioColor ;
+    }
 }
+
+
+
+//   old things that aren't relevant
+
+
+
+
+    //if ((document.getElementById('blue').checked == false) && (document.getElementById('green').checked == false) && (document.getElementById('pink').checked == false) && (document.getElementById('black').checked == false)) {
+    //    console.log("radio button: false");
+    //    document.getElementById('radio_selection').style.border = errorStyle;
+    //    return false;
+    //}
+    //
+    //if (document.getElementById('blue').checked) {
+    //    //radioValue = document.getElementById('blue').value;
+    //    document.body.style.backgroundColor = "#0bb3d0";
+    //
+    //}
+    //
+    //else if (document.getElementById('green').checked) {
+    //    //radioValue = document.getElementById('green').value;
+    //    document.body.style.backgroundColor = "#b4d433";
+    //
+    //}
+    //
+    //else if (document.getElementById('pink').checked) {
+    //    //radioValue = document.getElementById('pink').value;
+    //    document.body.style.backgroundColor = "#ff0099";
+    //
+    //}
+    //
+    //else if (document.getElementById('black').checked) {
+    //    //radioValue = document.getElementById('black').value;
+    //    document.body.style.backgroundColor = "#000";
+    //
+    //}
+
+    //switch (radioValue) {
+    //
+    //    case "Blue":
+    //        document.body.style.backgroundColor = "#0bb3d0";
+    //        break;
+    //
+    //    case "Green":
+    //        document.body.style.backgroundColor = "#b4d433";
+    //        break;
+    //
+    //    case "Pink":
+    //        document.body.style.backgroundColor = "#ff0099";
+    //        break;
+    //
+    //    case "Black":
+    //        document.body.style.backgroundColor = "#000";
+    //        break;
+    //
+    //    default:
+//
+//
+//    else {
+//        console.log("return: true");
+//        document.getElementById('result').textContent = "<h1>Thank you for joining, <br />" + fullName + "! <h1> <p>Please remember your log-in information for future discussions. <br />" + email + "<br />" + password + "</p>";
+//     }
+//}
