e8ba11e1379c9ab0462a1d71e4bafe62f14e50d7
Ralph Emrick <qwerty.ralph@gmail.com>
2016-03-14 16:20:24 -0400

    06_trying to implement jquery validation plugin (not working)
    trying to save time and it isn’t working -_-;

---
+++ 06 jquery /jquery.js
@@ -0,0 +1,85 @@
+$().ready(function() {
+
+    //
+    //$.validator.addMethod("alphaNumeric", function(value, element) {
+    //    return this.optional(element) || /^.*(?=.*\d)(?=.*[a-zA-Z]).*$/.test(value);
+    //}, "Username must contain a number");
+
+
+    $('#submission_form').validate ( {
+        //
+        //onkeyup: false,
+        //
+        //onfocusout: false,
+
+        rules: {
+            fullname: {
+                required: true,
+                minlength: 3
+            //},
+            //
+            //email: {
+            //    required: true,
+            //    minlength: 6,
+            //    email: true
+            //},
+            //
+            //password: {
+            //    required: true,
+            //    minlength: 10,
+            //    alphaNumeric: true
+            //},
+            //
+            //retype_password: {
+            //    required: true,
+            //    equalTo: '#password'
+            //},
+            //
+            //color: {
+            //    required: true
+            }
+        },
+
+        messages: {
+            fullname: {
+                required: "Please enter your full name",
+                minlength: "Must be more than 2 characters long"
+
+                //},
+                //
+                //email: {
+                //    required: "Please enter your email",
+                //    minlength: "Must be more than 5 characters long",
+                //    email: "Must be a valid email address"
+                //},
+                //
+                //password: {
+                //    required: "Please enter your password",
+                //    minlength: "Must be more than 10 characters long",
+                //    alphaNumeric: "Must contain a number"
+                //},
+                //
+                //retype_password: {
+                //    required: "Please retype the password above",
+                //    equalTo: "Passwords do not match"
+                //},
+                //
+                //color: {
+                //    required: "Please select your favorite color"
+            }
+
+            //errorPlacement: function ($error, $element) {
+            //    var name = $element.attr("name");
+            //
+            //    $(".error" + name).append($error);
+            //},
+            //
+        },
+
+        submitHandler: function(form) {
+            form.submit();
+        }
+
+
+        });
+});
