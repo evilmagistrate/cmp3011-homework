d18ca2b19c168491c272a4f99123880b85f71ae4
Ralph Emrick <qwerty.ralph@gmail.com>
2016-03-25 15:57:43 -0400

    07_set up continues

---
+++ 07 jquery plugins/navigation.js
@@ -0,0 +1,15 @@
+$().ready(function() {
+
+    $('nav a').css({
+        'font-size': "200%",
+        'margin': "12px"
+
+    });
+
+
+    $("nav").navigation({
+        maxWidth: "667px"
+    });
+
+
+});
