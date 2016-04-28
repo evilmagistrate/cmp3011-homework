b01ea45a85935a66655f44a114d2019e5b82fc8b
Ralph Emrick <qwerty.ralph@gmail.com>
2016-04-28 13:08:02 -0400

    Wild Flailing

--- 14 production /JS/ORZE.js
+++ 14 production /JS/ORZE.js
@@ -32,23 +32,40 @@ Basic requirements:
 header-
 back button (always returns to main list screen or just back one step?)
 logo (does anything?)
-hamburger (menu)
+//hamburger (menu)
 
-menu-
-checklist mode (toggle on/off)
-sort (also toggle?)
-lists (home)
-settings
+//menu-
+//checklist mode (toggle on/off)
+//sort (also toggle?)
+//lists (home)
+//settings
 
  */
 
-
 $().ready(function() {
 
+    var $itemList = $('#list_content');
+    var $newItem = $('#listItem');
     var $newList= $('#new_list');
     var $addItem= $('#add_item');
 
-    $('#add_item').hide();
+    $addItem.hide();
+
+
+
+//storage management ideas, super basic
+
+    //$(function() {
+    //    var listItem = document.getElementById('listItem');
+    //    $(listItem).blur(function() {
+    //        localStorage.setItem('todoData', this.textContent);
+    //    });
+    //    if ( localStorage.getItem('todoData') ) {
+    //        listItem.textContent = localStorage.getItem('todoData');
+    //    }
+    //});
+
+//accepting user input on 'return' key
 
     $('input[type=text]').on('keydown', function(e) {
         if (e.which == 13) {
@@ -62,6 +79,15 @@ $().ready(function() {
             $(newInput).focus();
         }
     });
+
+ //$('#return_to_lists').on('click', function(e) {
+ //    e.preventDefault();
+ //    var url = this.href;
+ //
+ //    $('#wrapper').remove();
+ //    $('#wrapper').load(url + ' #wrapper').hide().fadeIn('slow');
+ //
+ //   });
 
     $newList.on('click', function newListcreation() {
         //var newInput = document.createElement("INPUT");
@@ -85,29 +111,22 @@ $().ready(function() {
         var newInput = document.createElement("INPUT");
         document.getElementById("list_content").appendChild(newInput);
         $(newInput).attr('placeholder', 'New Item');
-        $(newInput).attr('type', 'text');
+        $(newInput).attr('type', 'text')
         $(newInput).addClass('active');
-        $(newInput).addClass('ellipses');
-        $(newInput).attr('id', 'listItem');
         $(newInput).focus();
     });
 
-    $('#listItem').on('blur', function newItemcompleted() {
-
-        $('#listItem').removeClass('active');
-        $('#listItem').addClass('passive');
+    $('.listItem').on('blur', function newItemcompleted() {
 
     });
+
+
 
 //Formstone Plugins
 
     $("nav").navigation ({
         gravity: "right",
         type: "overlay"
-    });
-
-    $("input[type=checkbox]").checkbox({
-        toggle: true
     });
 
 //end Formstone Plugins
