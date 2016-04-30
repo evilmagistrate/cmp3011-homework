02ee95f709a52d520a3ccc9c8f30e45cdf80b225
Ralph Emrick <qwerty.ralph@gmail.com>
2016-04-30 18:03:15 -0400

    The commit I shoulda done earlier

--- 14 production /JS/ORZE.js
+++ 14 production /JS/ORZE.js
@@ -46,48 +46,23 @@ $().ready(function() {
 
     var $itemList = $('#list_content');
     var $newItem = $('#listItem');
-    var $newList= $('#new_list');
+    var $newList= $('#generate_new_list');
     var $addItem= $('#add_item');
 
     $addItem.hide();
 
+    var listContents = [];
 
 
-//storage management ideas, super basic
+    function storeNewItem( item, quantity, category ) {
 
-    //$(function() {
-    //    var listItem = document.getElementById('listItem');
-    //    $(listItem).blur(function() {
-    //        localStorage.setItem('todoData', this.textContent);
-    //    });
-    //    if ( localStorage.getItem('todoData') ) {
-    //        listItem.textContent = localStorage.getItem('todoData');
-    //    }
-    //});
+        var listContents = JSON.parse(localStorage.getItem( 'listName' ) || []);
+        listContents.push({item: item , quantity: quantity, category: category });
+        localStorage.setItem( 'listName', JSON.stringify(listContents));
 
-//accepting user input on 'return' key
+    }
 
-    $('input[type=text]').on('keydown', function(e) {
-        if (e.which == 13) {
-            var newInput = document.createElement("INPUT");
-            document.getElementById("list_content").appendChild(newInput);
-            $(newInput).attr('placeholder', 'New Item');
-            $(newInput).attr('type', 'text');
-            $(newInput).addClass('active');
-            $(newInput).addClass('ellipses');
-            $(newInput).attr('id', 'listItem');
-            $(newInput).focus();
-        }
-    });
-
- //$('#return_to_lists').on('click', function(e) {
- //    e.preventDefault();
- //    var url = this.href;
- //
- //    $('#wrapper').remove();
- //    $('#wrapper').load(url + ' #wrapper').hide().fadeIn('slow');
- //
- //   });
+//creating content
 
     $newList.on('click', function newListcreation() {
         //var newInput = document.createElement("INPUT");
@@ -101,25 +76,57 @@ $().ready(function() {
 
     $newList.on('blur', function newListTitled() {
 
-        var newText = $newList.val();
         $newList.removeClass('active');
         $newList.addClass('blue-middle');
 
+        storeNewItem();
+
+    });
+
+//accepting user input on 'return' key
+
+    $('input[type=text]').on('keydown', function(e) {
+        if (e.which == 13) {
+
+            var newLI = document.createElement("LI");
+            var newInput = document.createElement("INPUT");
+            newLI.appendChild(newInput);
+            document.getElementById("list_content").appendChild(newLI);
+
+            $(newInput).attr('placeholder', 'New Item');
+            $(newInput).attr('type', 'text');
+
+            $(newInput).addClass( "listItem active" );
+
+            $(newInput).focus();
+
+        }
     });
 
     $addItem.on('click', function newItemfield() {
+        var newLI = document.createElement("LI");
         var newInput = document.createElement("INPUT");
-        document.getElementById("list_content").appendChild(newInput);
+        newLI.appendChild(newInput);
+        document.getElementById("list_content").appendChild(newLI);
+
+
         $(newInput).attr('placeholder', 'New Item');
-        $(newInput).attr('type', 'text')
-        $(newInput).addClass('active');
+        $(newInput).attr('type', 'text');
+
+        $(newInput).addClass( "listItem active" );
+
         $(newInput).focus();
+
     });
 
     $('.listItem').on('blur', function newItemcompleted() {
 
-    });
+        $(".listItem").removeClass( "active" );
 
+        var item = $(".listItem").value;
+
+
+    });
 
 
 //Formstone Plugins
@@ -130,6 +137,5 @@ $().ready(function() {
     });
 
 //end Formstone Plugins
-
 
 });
